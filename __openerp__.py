54016d07e638734e5ac81241b96d465f6058694c
root <root@fs-openerp.fs-factory>
2014-10-23 19:37:49 +0800

    new file:   __init__.py 	new file:   __openerp__.py 	new file:   im.py 	new file:   im_view.xml

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    "name" : "Instant messaging history",
+    "version" : "1.0",
+    "author" : "L",
+    "category" : "Generic Modules/Human Resources",
+    "website" : "-",
+    "description": "Adds history view to imstant messaging module.",
+    "depends" : ["im"],
+    "init_xml" : [],
+    "update_xml" : ["im_view.xml"],
+    "installable": True
+}
---
+++ im.py
@@ -0,0 +1,7 @@
+from openerp.osv import fields,osv
+
+class im_history(osv.osv):
+    _inherit = 'im.message'
+
+
+im_history()
