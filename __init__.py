05920ce9f384dbf20f036164d31b6f38ccf2a642
vrms <vrms@netcologne.de>
2014-10-28 07:06:11 +0800

    Update __init__.py
    reverted to same as 'saas1' & 'master'
    
    # replacing "import im_chat" with "import im.py" did not work. 
    # Just results a similar error when trying to install "there is no Module im"
    # instead of "there is no Modle im_chat"

--- __init__.py
+++ __init__.py
@@ -1,2 +1,6 @@
-#import im_chat
-import im.py
+import im_chat
+#
+# replacing "import im_chat" with "import im.py" did not work. 
+# Just results a similar error when trying to install "there is no Module im"
+# instead of "there is no Modle im_chat"
+#
