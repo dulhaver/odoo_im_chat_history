6b45219ce7c97c05d9edf65ea29d9bd0476f696f
vrms <vrms@netcologne.de>
2014-10-29 11:54:03 +0800

    Update __openerp__.py
    uncommented line 8   "depends" : ["im_chat"],   
    
    as suggested https://www.odoo.com/forum/help-1/question/v8-what-does-the-error-message-no-module-named-xyz-mean-66964

--- __openerp__.py
+++ __openerp__.py
@@ -5,7 +5,7 @@
     "category" : "Generic Modules/Messaging",
     "website" : "-",
     "description": "Adds history view to instant messaging module.",
-    "depends" : ["im_chat"],
+#    "depends" : ["im_chat"],
     "init_xml" : [],
     "update_xml" : ["im_view.xml"],
     "installable": True
