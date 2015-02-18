7c29ab74d5dadc53acf391c0d2d92a365ce552fa
vrms <vrms@netcologne.de>
2015-02-18 20:11:30 +0800

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -1,12 +1,13 @@
 {
-    "name" : "Instant messaging history",
-    "version" : "1.0",
-    "author" : "L",
-    "category" : "Generic Modules/Messaging",
-    "website" : "-",
+    "name": "Instant messaging history",
+    "version": "1.1",
+    "author": "Laura Jarvpaa",
+    "author": "Mohammed Roshan",
+    "category": "Generic Modules/Messaging",
+    "website": "-",
     "description": "Adds history view to instant messaging module.",
-    "depends" : ["im_chat"],
-    "init_xml" : [],
-    'data':["im_view.xml"],
+    "depends": ["im_chat"],
+    "init_xml": [],
+    'data': ["im_view.xml"],
     "installable": True
 }
