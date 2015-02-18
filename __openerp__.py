04660e412240b13e96895946470827ef39ebc0ac
vrms <vrms@netcologne.de>
2015-02-18 19:34:59 +0800

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -5,8 +5,8 @@
     "category" : "Generic Modules/Messaging",
     "website" : "-",
     "description": "Adds history view to instant messaging module.",
-#    "depends" : ["im_chat"],
+    "depends" : ["im_chat"],
     "init_xml" : [],
-    "update_xml" : ["im_view.xml"],
+    'data':["im_view.xml"],
     "installable": True
 }
